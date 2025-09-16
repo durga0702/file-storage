32deda7fb3501fbf382239171627898be99c630f
durga0702 <durgamay5@gmail.com>
2025-09-16 12:36:18 +0530

    file works

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+
+  /**
+   * file uploading
+   * @param file 
+   */
+  onFileUpload(file:any){
+   alert('Uploaded Successfully')
+  }
+
+  /**
+   * copy link
+   */
+  copyLink(){
+    navigator.clipboard.writeText('https://google.com')
+    .then(() => {
+      alert('Link copied to clipboard!');
+    })
+    .catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+  }
 }
